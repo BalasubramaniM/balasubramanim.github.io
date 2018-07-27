96eada2895969e2e3fd7ddc4c27ca43129fccf4b
Balasubramani M <bala.m@live.in>
2018-07-27 08:14:09 +0530

    Added certifications.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -26,4 +26,37 @@ $(document).ready(function() {
 				break;
 		}
 	});
+
+	$(".certifications .margin-bottom").on("click", function() {
+		switch (this.id) {
+			case "certificate1":
+				$(".certificate1")
+					.addClass("is-active")
+					.siblings()
+					.removeClass("is-active");
+				break;
+			case "certificate2":
+				$(".certificate2")
+					.addClass("is-active")
+					.siblings()
+					.removeClass("is-active");
+				break;
+			case "certificate3":
+				$(".certificate3")
+					.addClass("is-active")
+					.siblings()
+					.removeClass("is-active");
+				break;
+		}
+	});
+
+	$(".modal-close").on("click", function() {
+		$(this)
+			.parent()
+			.removeClass("is-active");
+	});
+
+	$(document).keyup(function(e) {
+		if (e.keyCode === 27) $(".modal").removeClass("is-active"); // esc key handling
+	});
 });
